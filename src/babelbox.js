8e2d6ce0af01f8cdb7d40dfac57172e8e7a6f575
Yasser Fadl <yasser.fadl@gmail.com>
2015-06-22 20:48:54 +0200

    Adding translate token/blobs functionality

--- src/babelbox.js
+++ src/babelbox.js
@@ -8,6 +8,8 @@
 	}
 }( this, function() {
 
+	var SPLIT_CHAR = '.';
+
 	function babelbox() {
 		this.tokens = {};
 	}
@@ -15,6 +17,43 @@
 	babelbox.prototype.addTokens = function( tokens ) {
 		this.tokens = deepmerge( this.tokens, tokens );
 	};
+
+	babelbox.prototype.blob = function( text ) {
+		var message;
+		var matches = text.match( /({{[^}]+}})/g );
+		for( var i = 0; i < matches.length; i++ ) {
+			message = getTokenValue( matches[ i ].substring( 2, matches[ i ].length - 2 ), this.tokens );
+			text = text.replace( matches[ i ], message );
+		}
+		return text;
+	};
+
+	babelbox.prototype.translate = function( token, mappings ) {
+		var translateToken = getTokenValue( token, this.tokens );
+		if( translateToken != null ) {
+			for( var key in mappings ) {
+				translateToken = translateToken.replace( "{{" + key + "}}", mappings[ key ] );
+			}
+		}
+		return translateToken;
+	};
+
+	function getTokenValue( token, tokens ) {
+		return objectCrawl( token.split( SPLIT_CHAR ), tokens, "", 0 );
+	}
+
+	function objectCrawl( parts, tokens, translation, iteration ) {
+		var lastpart = parts[ parts.length - 1 ];
+		var part = parts[ iteration ];
+		if( tokens[ lastpart ] ) {
+			translation = tokens[ lastpart ]
+		}
+		if( iteration < parts.length - 1 && tokens[ part ] ) {
+			return objectCrawl( parts, tokens[ part ], translation, iteration + 1 );
+		} else {
+			return translation || null;
+		}
+	}
 
 	function deepmerge( target, src, eventHub ) {
 		var dst = {};
---
+++ test/translatingSingleTokenSpec.js
@@ -0,0 +1,54 @@
+describe( "adding tokens", function() {
+
+	var i18n;
+	beforeEach( function() {
+		i18n = new babelbox();
+		i18n.addTokens( {
+			confirmed: 'It\'s confirmed!',
+			please_confirm: 'Please confirm your booking',
+			booking: {
+				confirmed: 'Your booking no:{{booking_no}} on {{day}} was succesfully confirmed!',
+				to_confirm: 'Your booking will be confirmed shortly!',
+				hotel: {
+					confirmed: 'Your hotel booking at {{hotel}} is confirmed!',
+					to_confirm: 'Your hotel booking will be confirmed shortly!',
+				}
+			}
+		} );
+	} );
+
+	it( "can get a top level translation", function() {
+		expect( i18n.translate( 'confirmed' ) ).toEqual( 'It\'s confirmed!' );
+	} );
+
+	it( "can get a translation one level deep", function() {
+		expect( i18n.translate( 'booking.to_confirm' ) ).toEqual( 'Your booking will be confirmed shortly!' );
+	} );
+
+	it( "can get a translation two levels deep", function() {
+		expect( i18n.translate( 'booking.hotel.to_confirm' ) ).toEqual( 'Your hotel booking will be confirmed shortly!' );
+	} );
+
+	it( "can gets a booking one level higher when not available", function() {
+		expect( i18n.translate( 'booking.please_confirm' ) ).toEqual( 'Please confirm your booking' );
+	} );
+
+	it( "can gets a booking two levels higher when not available", function() {
+		expect( i18n.translate( 'booking.hotel.please_confirm' ) ).toEqual( 'Please confirm your booking' );
+	} );
+
+	it( "can replace a token correctly in translation", function() {
+		expect( i18n.translate( 'booking.hotel.confirmed', {
+			hotel: 'TheHotel'
+		} ) ).toEqual( 'Your hotel booking at TheHotel is confirmed!' );
+	} );
+
+	it( "can replace multiple tokens correctly in translation", function() {
+		expect( i18n.translate( 'booking.confirmed', {
+			booking_no: '12432',
+			day: 'Sunday'
+		} ) ).toEqual( 'Your booking no:12432 on Sunday was succesfully confirmed!' );
+	} );
+
+
+} );
