29904f3e4c69a76e04b3918faad027422874c118
Yasser Fadl <yasser.fadl@gmail.com>
2015-11-13 16:34:04 +0100

    Adding slight improvements to babelbox example

--- example/requirejs/scripts/main.js
+++ example/requirejs/scripts/main.js
@@ -2,7 +2,7 @@ requirejs.config( {
 	paths: {
 		'jquery': 'https://code.jquery.com/jquery-1.11.3.min',
 		'text': 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
-		'babelbox': './babelbox',
+		'babelbox': 'https://rawgit.com/hoxton-one/babelbox-requirejs/master/babelbox',
 		'i18n': 'https://rawgit.com/hoxton-one/babelbox/master/dist/babelbox.min'
 	},
 	babelbox: {
